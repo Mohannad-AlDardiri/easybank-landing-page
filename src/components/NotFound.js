37f04f019f4f1e6db8c828989c3d28188d4e46d1
mohaned mohamed <mhnd62864@gmail.com>
2021-12-15 13:37:47 +0200

    problems

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react'
 const NotFound = () => {
     return (
         <div className="flex justify-center items-center p-12 w-full h-full ">
-            <h1 className="text-7xl " style={{padding: "3rem"}}>This website doesn't include other pages because it's just a design for an unreal bank.</h1>   
+            <h1 className="text-3xl lg:text-7xl " style={{padding: "3rem"}}>This website doesn't include other pages because it's just a design for an unreal bank.</h1>   
         </div>
     )
 }
