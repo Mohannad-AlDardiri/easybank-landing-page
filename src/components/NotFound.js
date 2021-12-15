efd924ab145604ec17e6a1bdb838323dd49c71c0
mohaned mohamed <mhnd62864@gmail.com>
2021-12-15 13:57:47 +0200

    fixed errors

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotFound = () => {
     return (
-        <div className="flex justify-center items-center p-12 w-full h-full ">
+        <div className="flex justify-center items-center p-6 lg:p-12 w-full h-full ">
             <h1 className="text-3xl lg:text-7xl " style={{padding: "3rem"}}>This website doesn't include other pages because it's just a design for an unreal bank.</h1>   
         </div>
     )
